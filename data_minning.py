9d0f0c9d7ab06e8123587cb2621aeee3d9f63db8
zhout3 <tianxin@folsom-wl-95.dynamic2.rpi.edu>
2017-03-28 22:24:59 -0400

    Beautify the code.
    Beautify the code.

--- data_minning.py
+++ data_minning.py
@@ -4,31 +4,49 @@ import operator
 import validators
 import json
 import tweepy
+#Adding your api key and secret here
 api_key = '2hNqzcWDgUdZy4xBqhB5QZOW1'
 api_secret = '7h83YOhKAhBSZDguPW1KLpcuzhCaE5q09qcXruweoKjYA6Qhtd'
 
+#Adding your api token and secret here.
 access_token = '834824567864582144-j64sQIlJPeVxRbHn7JRpuCFqyfGFiHO'
 access_token_secret = 'Y1udhZoHNN7sKdkov221VoTDWieFpQr3VAgjFQQoe0gCF'
 
-
-
+#Connecting twitter API
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 
+#Get the api
 api = tweepy.API(auth)
 
-frequency = defaultdict(int)
-for tweet in tweepy.Cursor(api.search, q="http://www.foxnews.com/politics/2017/03/27/sessions-takes-aim-at-dangerous-sanctuary-cities-warns-on-funding.html", rpp=100, count=20, result_type="recent", include_entities=True, lang="en").items(100):
-   temp = tweet.entities.get("urls")
-   for item in temp:
-       frequency[item.get('url')]+=1
+#Function to catch urls from database.
+def catch_url(input):
+    frequency = defaultdict(int)
+    for tweet in tweepy.Cursor(api.search, q=input, rpp=100, count=20, result_type="recent", include_entities=True, lang="en").items(100):
+        temp = tweet.entities.get("urls")
+        for item in temp:
+            frequency[item.get('url')]+=1
+    sorted_x = sorted(frequency.items(), key=operator.itemgetter(1),reverse=True)
+    return sorted_x
 
-sorted_x = sorted(frequency.items(), key=operator.itemgetter(1),reverse=True)
+#Function to catch hashtags from database.
+def catch_hashtags(input):
+    frequency_hash = defaultdict(int)
+    for tweet in tweepy.Cursor(api.search,
+                               q=input,
+                               rpp=100, count=20, result_type="recent", include_entities=True, lang="en").items(100):
+        print(tweet.text, tweet.entities.get("hashtags"))
+        temp = tweet.entities.get("hashtags")
+        if temp is not None:
+            for item in temp:
+                if item is not None:
+                    frequency_hash[item.get('text')] += 1
+    sorted_x = sorted(frequency_hash.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_x
 
 
-
-
-
+#Testing catch_url.
+sorted_x = catch_url("http://www.theverge.com/2017/3/27/15077864/elon-musk-neuralink-brain-computer-interface-ai-cyborgs")
 file =open("urls.txt","w")
 for x in range(len(sorted_x)):
     file.write(str(sorted_x[x][0]))
@@ -36,21 +54,8 @@ for x in range(len(sorted_x)):
     file.write("\n")
 
 
-
-
-frequency_hash = defaultdict(int)
-for tweet in tweepy.Cursor(api.search, q="#google", rpp=100, count=20, result_type="recent", include_entities=True, lang="en").items(10):
-   temp = tweet.entities.get("hashtags")
-   
-   if temp is not None:
-    for item in temp:
-       if item is not None:
-        frequency_hash[item.get('text')]+=1
-
-
-sorted_x = sorted(frequency_hash.items(), key=operator.itemgetter(1),reverse=True)
-
-
+#Testing catch_hashtags.
+sorted_x = catch_hashtags("http://www.theverge.com/2017/3/27/15077864/elon-musk-neuralink-brain-computer-interface-ai-cyborgs")
 file=open("hashtag.txt","w")
 for x in range(len(sorted_x)):
     file.write(str(sorted_x[x][0]))
